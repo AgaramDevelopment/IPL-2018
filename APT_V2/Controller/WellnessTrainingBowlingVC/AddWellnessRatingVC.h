660b67c6b525efb3f8cbb6ab84ed47cc194482d1
Apple <apple@Apples-MacBook-Pro.local>
2018-02-07 14:26:39 +0530

    wellness commit

---
+++ APT_V2/Controller/WellnessTrainingBowlingVC/AddWellnessRatingVC.h
@@ -0,0 +1,13 @@
+//
+//  AddWellnessRatingVC.h
+//  APT_V2
+//
+//  Created by Apple on 07/02/18.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddWellnessRatingVC : UIViewController
+
+@end
